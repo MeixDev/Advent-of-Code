067189cf2934940d02266e74fed0980c79b372e1
Meï (meixdev) <main.meixdev@gmail.com>
2022-12-03 14:55:04 +0100

    Added the `characters` package. Added an ASCII enum to help readability

--- utils/index.dart
+++ utils/index.dart
@@ -1,9 +1,11 @@
+export 'ascii.dart';
 export 'generic_day.dart';
 export 'input_util.dart';
 export 'parse_util.dart';
 export 'field.dart';
 
 // package auto imports
+export 'package:characters/characters.dart';
 export 'package:collection/collection.dart';
 export 'package:tuple/tuple.dart';
 export 'package:quiver/core.dart';
