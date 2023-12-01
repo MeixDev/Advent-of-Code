4874c9e634ff4f862c5c4a8a369c377aa533a2d3
MeixDev <main.meixdev@gmail.com>
2023-12-01 05:56:26 +0100

    Groundwork for AoC2023

---
+++ 2023/solutions/day01.dart
@@ -0,0 +1,22 @@
+import '../utils/index.dart';
+
+class Day01 extends GenericDay {
+  Day01() : super(1);
+
+  @override
+  void parseInput() {
+    final inputUtil = InputUtil(1);
+    final lines = inputUtil.getPerLine();
+    // Do things
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
