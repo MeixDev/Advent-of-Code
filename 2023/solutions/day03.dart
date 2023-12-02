23c7859994f8063395281b71ff9a8489d497e8e9
MeixDev <main.meixdev@gmail.com>
2023-12-02 06:31:24 +0100

    Day02 2023 - Cubes go brrr

---
+++ 2023/solutions/day03.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day03 extends GenericDay {
+  Day03() : super(3);
+
+  void parseLines(List<String> lines) {}
+
+  @override
+  void parseInput() {
+    final inputUtil = InputUtil(day);
+    final lines = inputUtil.getPerLine();
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
