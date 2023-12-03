1b0c71e9aa65b9752e138abe6a8d0c329ccffe88
MeixDev <main.meixdev@gmail.com>
2023-12-03 06:37:45 +0100

    Day03 2023 - Map those symbols out, will ya

---
+++ 2023/solutions/day04.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day04 extends GenericDay {
+  Day04() : super(4);
+
+  void parseLines(List<String> lines) {}
+
+  @override
+  void parseInput() {
+    final inputUtil = InputUtil(day);
+    final lines = inputUtil.getPerLine();
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
