dc1f795cf0886900bcbafe8dc3a85797abd456f5
MeixDev <main.meixdev@gmail.com>
2023-12-05 06:56:14 +0100

    Day05 - It's all about trusting the process. I should optimize this...

---
+++ 2023/solutions/day06.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day06 extends GenericDay {
+  Day06() : super(6);
+
+  void parseLines(List<String> lines) {}
+
+  @override
+  void parseInput() {
+    // ignore: unused_local_variable
+    final lines = input.getPerLine();
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
