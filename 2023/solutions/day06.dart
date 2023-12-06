07c6de75f0a0c83835a0d6ab321799754bdd7e8d
MeixDev <main.meixdev@gmail.com>
2023-12-06 06:16:59 +0100

    Day06 2023 - Parsing ? What's this ? I can only race

--- 2023/solutions/day06.dart
+++ 2023/solutions/day06.dart
@@ -1,23 +1,96 @@
 import '../utils/index.dart';
 
+class Race {
+  final int time;
+  final int distance;
+
+  Race({required this.time, required this.distance});
+}
+
 class Day06 extends GenericDay {
   Day06() : super(6);
 
-  void parseLines(List<String> lines) {}
-
   @override
-  void parseInput() {
-    // ignore: unused_local_variable
-    final lines = input.getPerLine();
+  List<Race> parseInput() {
+    final races = <Race>[];
+    // Input Races:
+    races.add(Race(time: 54, distance: 446));
+    races.add(Race(time: 81, distance: 1292));
+    races.add(Race(time: 70, distance: 1035));
+    races.add(Race(time: 88, distance: 1007));
+
+    // Example Races:
+    // races.add(Race(time: 7, distance: 9));
+    // races.add(Race(time: 15, distance: 40));
+    // races.add(Race(time: 30, distance: 200));
+    return races;
+  }
+
+  Race parseInputP2() {
+    // Input Race:
+    return Race(time: 54817088, distance: 446129210351007);
+
+    // Example Race:
+    // return Race(time: 71530, distance: 940200);
   }
 
   @override
   int solvePart1() {
-    return 0;
+    final races = parseInput();
+    int score = 1;
+    for (final race in races) {
+      int x = 0;
+      int y = race.time;
+      while (true) {
+        final speed = x;
+        final time = race.time - x;
+        final distance = speed * time;
+        if (distance > race.distance) {
+          break;
+        }
+        x++;
+      }
+      while (true) {
+        final speed = y;
+        final time = race.time - y;
+        final distance = speed * time;
+        if (distance > race.distance) {
+          break;
+        }
+        y--;
+      }
+      final totalSolutions = y - x + 1;
+      score *= totalSolutions;
+    }
+    return score;
   }
 
   @override
   int solvePart2() {
-    return 0;
+    final race = parseInputP2();
+    int score = 1;
+    int x = 0;
+    int y = race.time;
+    while (true) {
+      final speed = x;
+      final time = race.time - x;
+      final distance = speed * time;
+      if (distance > race.distance) {
+        break;
+      }
+      x++;
+    }
+    while (true) {
+      final speed = y;
+      final time = race.time - y;
+      final distance = speed * time;
+      if (distance > race.distance) {
+        break;
+      }
+      y--;
+    }
+    final totalSolutions = y - x + 1;
+    score *= totalSolutions;
+    return score;
   }
 }
