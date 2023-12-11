2a41826788d8ad06f1807242378be56f85e5c873
MeixDev <main.meixdev@gmail.com>
2023-12-11 06:23:39 +0100

    Day11 2023 - I wish Galaxy 3 would come out already

---
+++ 2023/solutions/day12.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day12 extends GenericDay {
+  Day12() : super(12);
+
+  void parseLines(List<String> lines) {}
+
+  @override
+  void parseInput() {
+    // ignore: unused_local_variable
+    final lines = input.getPerLine();
+  }
+
+  @override
+  int solvePart1() {
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+    return 0;
+  }
+}
