2f4efddbcf5c857c76b78f04597075d21b447b02
MeixDev <main.meixdev@gmail.com>
2024-12-01 04:07:36 +0100

    Pre-Day01 2024 - Setup

---
+++ 2024/solutions/index.dart
@@ -0,0 +1,28 @@
+// This is an index file for grouped exports of all solutions. It is used to
+// avoid cluttering main with imports for every day. Additionally, it makes
+// boilerplate code creation with the CLI easier.
+export 'day01.dart';
+// export 'day02.dart';
+// export 'day03.dart';
+// export 'day04.dart';
+// export 'day05.dart';
+// export 'day06.dart';
+// export 'day07.dart';
+// export 'day08.dart';
+// export 'day09.dart';
+// export 'day10.dart';
+// export 'day11.dart';
+// export 'day12.dart';
+// export 'day13.dart';
+// export 'day14.dart';
+// export 'day15.dart';
+// export 'day16.dart';
+// export 'day17.dart';
+// export 'day18.dart';
+// export 'day19.dart';
+// export 'day20.dart';
+// export 'day21.dart';
+// export 'day22.dart';
+// export 'day23.dart';
+// export 'day24.dart';
+// export 'day25.dart';
