c7b838fde0560875434c0a83caaaffc1150f9761
MeixDev <main.meixdev@gmail.com>
2024-12-01 07:12:36 +0100

    Day01 2024 - Look at this idiot who woke up late.

--- 2024/solutions/day01.dart
+++ 2024/solutions/day01.dart
@@ -3,21 +3,55 @@ import '../utils/index.dart';
 class Day01 extends GenericDay {
   Day01() : super(1);
 
-  void parseLines(List<String> lines) {}
+  Tuple2<List<int>, List<int>> parseLines(List<String> lines) {
+    final List<int> list1Numbers = [];
+    final List<int> list2Numbers = [];
+    for (final line in lines) {
+      final numbers = line.split('   ');
+      list1Numbers.add(int.parse(numbers[0]));
+      list2Numbers.add(int.parse(numbers[1]));
+    }
+    return Tuple2(list1Numbers, list2Numbers);
+  }
 
   @override
-  void parseInput() {
-    // ignore: unused_local_variable
+  Tuple2<List<int>, List<int>> parseInput() {
     final lines = input.getPerLine();
+    final parsedLines = parseLines(lines);
+    return parsedLines;
+  }
+
+  int comparator(int a, int b) {
+    return a.compareTo(b);
   }
 
   @override
   int solvePart1() {
-    return 0;
+    final Tuple2<List<int>, List<int>> numbers = parseInput();
+    final List<int> list1 = numbers.item1.sorted(comparator);
+    final List<int> list2 = numbers.item2.sorted(comparator);
+    int index = 0;
+    int sum = 0;
+    while (index < list1.length) {
+      sum += (list1[index] - list2[index]).abs();
+      index++;
+    }
+    return sum;
   }
 
   @override
   int solvePart2() {
-    return 0;
+    final Tuple2<List<int>, List<int>> numbers = parseInput();
+    final List<int> list1 = numbers.item1.sorted(comparator);
+    final List<int> list2 = numbers.item2.sorted(comparator);
+    int index = 0;
+    int sum = 0;
+    while (index < list1.length) {
+      final int count =
+          list2.where((element) => element == list1[index]).length;
+      sum += list1[index] * count;
+      index++;
+    }
+    return sum;
   }
 }
