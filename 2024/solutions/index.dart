8bc08ef92dcdd1e3d7f21fee2121d5b21302035c
MeixDev <main.meixdev@gmail.com>
2024-12-02 06:42:45 +0100

    Day02 2024 - Once again lost 20 minutes remaking the whole thing for P2

--- 2024/solutions/index.dart
+++ 2024/solutions/index.dart
@@ -2,8 +2,8 @@
 // avoid cluttering main with imports for every day. Additionally, it makes
 // boilerplate code creation with the CLI easier.
 export 'day01.dart';
-// export 'day02.dart';
-// export 'day03.dart';
+export 'day02.dart';
+export 'day03.dart';
 // export 'day04.dart';
 // export 'day05.dart';
 // export 'day06.dart';
